3ade6f3033ea9c8140cf7c7e6995b32397599bc1
Zero <90512731+ZeroTRGG@users.noreply.github.com>
2021-09-25 16:09:06 +0300

    Add files via upload

---
+++ foot.py
@@ -0,0 +1,80 @@
+class Football:
+
+    def team1(self):
+        print('КАНЕВЦЕВ')
+    def team2(self):
+        print('МИЗЮК')
+    def team3(self):
+        print('ШОПИН')
+    def team4(self):
+        print('НЕХТИЙ')
+    def team5(self):
+        print('ГОРБУНОВ')
+    def team6(self):
+        print('ДЕМЧЕНКО')
+    def team7(self):
+        print('ЛЮЛЬКА')
+    def team8(self):
+        print('САРАПИЙ')
+    def team9(self):
+        print('ФОМИН')
+    def team10(self):
+        print('РОМАНЮК')
+    def team11(self):
+        print('ПОРОХ')
+    def num1(self):
+        print('1')
+    def num2(self):
+        print('2')
+    def num3(self):
+        print('27')
+    def num4(self):
+        print('7')
+    def num5(self):
+        print('20')
+    def num6(self):
+        print('17')
+    def num7(self):
+        print('22')
+    def num8(self):
+        print('5')
+    def num9(self):
+        print('86')
+    def num10(self):
+        print('47')
+    def num11(self):
+        print('31')
+    
+
+
+
+def main():
+    foot=Football()
+    foot.team1()
+    foot.team2()
+    foot.team3()
+    foot.team4()
+    foot.team5()
+    foot.team6()
+    foot.team7()
+    foot.team8()
+    foot.team9()
+    foot.team10()
+    foot.team11()
+    foot.num1()
+    foot.num2()
+    foot.num3()
+    foot.num4()
+    foot.num5()
+    foot.num6()
+    foot.num7()
+    foot.num8()
+    foot.num9()
+    foot.num10()
+    foot.num11()
+main() 
+
+
+
+
+    
