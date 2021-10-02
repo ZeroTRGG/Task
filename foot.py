29c24f7a85504892b79ce3cddfdb51841e324b34
Zero <90512731+ZeroTRGG@users.noreply.github.com>
2021-10-02 10:33:54 +0300

    Update foot.py

--- foot.py
+++ foot.py
@@ -1,26 +1,50 @@
 class Football:
+
     def team1(self):
-        print('КАНЕВЦЕВ', 'номер 1')
+        print('КАНЕВЦЕВ')
     def team2(self):
-        print('МИЗЮК', 'номер 2')
+        print('МИЗЮК')
     def team3(self):
-        print('ШОПИН', 'номер 3')
+        print('ШОПИН')
     def team4(self):
-        print('НЕХТИЙ', 'номер 4')
+        print('НЕХТИЙ')
     def team5(self):
-        print('ГОРБУНОВ', 'номер 5')
+        print('ГОРБУНОВ')
     def team6(self):
-        print('ДЕМЧЕНКО', 'номер 6')
+        print('ДЕМЧЕНКО')
     def team7(self):
-        print('ЛЮЛЬКА', 'номер 7')
+        print('ЛЮЛЬКА')
     def team8(self):
-        print('САРАПИЙ', 'номер 8')
+        print('САРАПИЙ')
     def team9(self):
-        print('ФОМИН', 'номер 9')
+        print('ФОМИН')
     def team10(self):
-        print('РОМАНЮК', 'номер 10')
+        print('РОМАНЮК')
     def team11(self):
-        print('ПОРОХ', 'номер 11')
+        print('ПОРОХ')
+    def num1(self):
+        print('1')
+    def num2(self):
+        print('2')
+    def num3(self):
+        print('27')
+    def num4(self):
+        print('7')
+    def num5(self):
+        print('20')
+    def num6(self):
+        print('17')
+    def num7(self):
+        print('22')
+    def num8(self):
+        print('5')
+    def num9(self):
+        print('86')
+    def num10(self):
+        print('47')
+    def num11(self):
+        print('31')
+
 
 def main():
     foot=Football()
@@ -35,4 +59,15 @@ def main():
     foot.team9()
     foot.team10()
     foot.team11()
+    foot.num1()
+    foot.num2()
+    foot.num3()
+    foot.num4()
+    foot.num5()
+    foot.num6()
+    foot.num7()
+    foot.num8()
+    foot.num9()
+    foot.num10()
+    foot.num11()
 main()
